e2e6477cb3cb56234f67c98e53660b10ce362b8b
hassancodess <dev.hassancodes@gmail.com>
2022-07-20 12:18:19 +0500

    Switch to Context API

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,26 @@ import Footer from './components/layout/Footer'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import NotFound from './Pages/NotFound'
+import GithubProvider from './context/github/GithubProvider'
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
 
-        <div className='container mx-auto pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
+          <div className='container mx-auto pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
+          </div>
+
+          <Footer />
         </div>
-
-        <Footer />
-      </div>
-    </Router>
+      </Router>
+    </GithubProvider>
   )
 }
 

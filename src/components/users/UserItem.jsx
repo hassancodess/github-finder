0d32c3568d5b65ea5e4aaeebaac36b87656083a6
hassancodess <dev.hassancodes@gmail.com>
2022-07-22 18:25:01 +0500

    add some changes

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -2,16 +2,16 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 function UserItem({ user: { login, avatar_url } }) {
   return (
-    <div class='card w-64 bg-base-100 shadow-xl'>
-      <div class='card-body'>
-        <div class='avatar justify-center'>
-          <div class='w-20 rounded-xl'>
+    <div className='card w-64 bg-base-100 shadow-xl'>
+      <div className='card-body'>
+        <div className='avatar justify-center'>
+          <div className='w-20 rounded-xl'>
             <img src={avatar_url} />
           </div>
         </div>
-        <h2 class='card-title justify-center'>{login}</h2>
-        <div class='card-actions justify-center'>
-          <button class='btn btn-primary btn-sm glass text-white'>
+        <h2 className='card-title justify-center'>{login}</h2>
+        <div className='card-actions justify-center'>
+          <button className='btn btn-secondary btn-sm text-white'>
             <Link to={`/users/${login}`}>View Profile</Link>
           </button>
         </div>
